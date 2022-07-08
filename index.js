274b8bd9e83df237c76dc63c991ff57122e4a47b
vikehieto <vikehietoyhoshii@gmail.com>
2022-07-08 18:45:01 +0530

    socket connection setup

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const app = require("express")();
 const env = require("dotenv");
-const path = require("path");
-const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const http = require("http").createServer(app);
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const { verify } = require("./middleware/verify");
 
@@ -24,10 +24,36 @@ const PORT = process.env.APP_PORT;
 // DB connection
 mongoose.connect(process.env.DB_URL, () => console.log("db connected..."));
 
+app.use(cors());
+const socket = require("socket.io")(http, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "DELETE"],
+
+    handlePreFlightRequest: (req, res) => {
+      res.writeHead(200, {
+        "Access-Control-Allow-Origin": process.env.CLIENT_URL,
+        "Access-Control-Allow-Methods": '["GET", "POST", "DELETE"]',
+      });
+    },
+  },
+});
+
 // ROUTES
 const authRouter = require("./routers/authRouter");
 const contactRouter = require("./routers/contactRouter");
 const User = require("./models/User");
+
+socket.on("connection", (socket) => {
+  console.log("socket connected...");
+
+  socket.on("yes", () => {
+    socket.emit("working", { response: "event received" });
+  });
+  socket.on("disconnect", () => {
+    console.log("socket disconnected");
+  });
+});
 
 app.use("/auth", authRouter);
 app.use("/contacts", verify, contactRouter);
@@ -51,4 +77,4 @@ app.get("/deletecontacts", verify, async (req, res) => {
   res.send("dodne");
 });
 
-app.listen(8000);
+http.listen(process.env.PORT || 8000);
