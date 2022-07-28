a4943c4f894fc47469391acbde4960070649c824
vikehieto <vikehietoyhoshii@gmail.com>
2022-07-28 12:46:27 +0530

    minor update

--- socketEventHandlers/messageDelivered.js
+++ socketEventHandlers/messageDelivered.js
@@ -1,14 +1,15 @@
 const { markAsDelivered } = require("../controllers/messageController");
 
 exports.handleMessageDelivered = async (socket, data, userIdSocketIdMap) => {
+  const time = !data.time ? data.delivered : data.time;
   // update message in database as delivered
-  const message = await markAsDelivered(data._id, data.time);
+  const message = await markAsDelivered(data._id, time);
   // emit to author of message that it was delivered
   try {
     socket.to(userIdSocketIdMap[data.authorId]).emit("marked-as-delivered", {
       chatId: data.chatId,
       messageId: data._id,
-      time: data.time,
+      time: time,
     });
   } catch (err) {
     console.log(err);
